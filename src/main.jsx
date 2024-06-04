8eec5684e5fef1b728aa40419697f78b6179b84d
Your Name <htan8657@gmail.com>
2024-06-04 08:40:46 +0700

    Revert "Front_End"
    This reverts commit bd256cb66bdb6a9d65c03da52aac98b4e5d9e4b0.

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,10 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import './index.css'
-// import LoginForm from './App.jsx'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <LoginForm />
-//   </React.StrictMode>,
-// )
-
-import React from "react";
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Include } from './Components/HomePage/Include.jsx';
-// Importing the Bootstrap CSS
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<Include  />, rootElement);
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Include />
+    <App />
   </React.StrictMode>,
 )
