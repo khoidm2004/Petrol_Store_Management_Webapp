2ac4c6f63942247e13d5ee161a8d1f29b0285486
khoidm2004 <khoidm2004@gmail.com>
2024-06-11 14:19:54 +0700

    Updated timeConverter

--- src/utils/timeConverter.js
+++ src/utils/timeConverter.js
@@ -1,7 +1,13 @@
 export const timeConverter = (timestamp) => {
-  const orgTimestamp = new Date(timestamp);
-  const date = orgTimestamp.toLocaleDateString();
-  const time = orgTimestamp.toLocaleDateString();
-
-  return { date, time };
+  if (typeof timestamp !== "number" || timestamp.toString().length !== 13) {
+    return { Title: "Error", Message: "Invalid timestamp", Status: "error" };
+  }
+  try {
+    const orgTimestamp = new Date(timestamp);
+    const date = orgTimestamp.toLocaleDateString();
+    const time = orgTimestamp.toLocaleTimeString();
+    return { date, time };
+  } catch (error) {
+    return { Title: "Error", Message: error.message, Status: "error" };
+  }
 };
