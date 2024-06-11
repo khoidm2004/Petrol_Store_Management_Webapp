181fbf5015e5cbcf4b5dcfb6bf8dd658781d9c61
khoidm2004 <khoidm2004@gmail.com>
2024-06-11 16:16:49 +0700

    Replace enviroment variable

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgAEWOMRs6xPSulEiXYZgucePF6O3FXaM",
-  authDomain: "petrol-b65ed.firebaseapp.com",
-  projectId: "petrol-b65ed",
-  storageBucket: "petrol-b65ed.appspot.com",
-  messagingSenderId: "440096217965",
-  appId: "1:440096217965:web:7685ed6565e8c70bfa1864",
-  measurementId: "G-DV4DF3BEZK",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
